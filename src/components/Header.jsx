643fc1b2f140e4a4cdf49ed4102d1a19bcfa1c9a
Andres Rico <andresrico07@gmail.com>
2022-09-12 09:16:25 -0500

    Header design

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Logo from '../assets/img/logo.png'
+import DarkModeSwitch from '../assets/img/moon-yellow-1.png'
+
+export const Header = () => {
+  return (
+    <header className='h-14 w-full bg-black-secundary flex flex-row justify-between'>
+        <div className='flex'>
+            <img src={Logo} className='h-16' alt="Logo" />
+            <img src={DarkModeSwitch} className='h-8 m-3' alt="Dark Mode" />
+
+        </div>
+        
+        <div className='flex items-center'>
+            <p className='font-fw-light text-white-primary px-3'>Perfil</p>
+            <p className='font-fw-light text-white-primary px-3'>Estudios</p>
+            <p className='font-fw-light text-white-primary px-3'>Skills</p>
+            <p className='font-fw-light text-white-primary px-3'>Experiencia</p>
+            <p className='font-fw-light text-white-primary px-3'>Proyectos</p>
+            <p className='font-fw-light text-white-primary px-3 mr-8'>Contáctame</p>
+        </div>
+        
+        
+    </header>
+  )
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.jsx"],
+  theme: {
+    extend: {},
+    colors:{
+      'black-primary': '#1E1E28',
+      'black-secundary': '#353B48',
+      'gray-primary': '#898A89',
+      'yellow-primary':'#FBC531',
+      'white-primary':'#FFFFFF'
+    },
+    fontWeight: {
+      'fw-light': 300,
+      'fw-regular': 400,
+      'fw-medium': 500,
+    },
+    fontFamily: {
+      'Roboto': ['"Roboto"', 'sans-serif']
+    }
+  },
+  plugins: [],
+}
