c7d68c3b725f52232722a63f99bc8f8439a9f0d4
Andres Rico <andresrico07@gmail.com>
2022-09-16 14:35:16 -0500

    get project from function. mobile first projects.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import ProjectsIcon from "../assets/img/projects-icon.png";
-import TrelloIcon from "../assets/img/trello-icon.png";
+import TrelloIcon from "../assets/img/trello-icon2.png";
+import { projectsData } from "../data/ProjectsData";
+import { CardProjects } from "./cards/CardProjects";
 
 export const Projects = () => {
+  //console.log(projectsData[0].title);
+
   return (
     <>
       <section className="mt-10 mx-5 sm:mx-20 md:mx-32 xl:mx-48 py-10 sm:py-14 md:py-20">
@@ -13,39 +17,16 @@ export const Projects = () => {
           </p>
         </div>
 
-        <div className="grid gap-4 mt-10 md:grid-cols-2 lg:grid-cols-3 justify-items-center">
-          <div className="bg-yellow-primary w-80 h-72 flex flex-col rounded-xl">
-            <p className="text-3xl text-black-secundary text-center mt-6">
-              Clone de Trello
-            </p>
-            <p className="text-black-secundary px-10 text-center mt-3">
-              Desarrollado con HTML, CSS y Javascript
-            </p>
-
-            <img
-              src={TrelloIcon}
-              className="h-auto w-9/12 m-1 mx-auto"
-              alt=""
+        <div className="grid gap-4 mt-10 grid-cols-1 justify-items-center w-full">
+          {projectsData.map((project) => (
+            <CardProjects
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              capture={project.capture}
+              developed_in={project.developed_in}
             />
-          </div>
-
-          
-            <div className="bg-yellow-primary w-80 h-72 flex flex-col rounded-xl">
-              <p className="text-3xl text-black-secundary text-center mt-6">
-                API Super Heroes
-              </p>
-              <p className="text-black-secundary px-10 text-center mt-3">
-                Desarrollado con ReactJS y Axios
-              </p>
-
-              <img
-                src={TrelloIcon}
-                className="h-auto w-9/12 m-1 mx-auto"
-                alt=""
-              />
-            </div>
-
-       
+          ))}
         </div>
 
         <span className="border-b w-full flex text-black-secundary mt-10"></span>
---
+++ src/data/ProjectsData.js
@@ -0,0 +1,34 @@
+export const projectsData = [
+  {
+    id: 1,
+    title: "Clone de Trello",
+    description:
+      "Aplicación que permite la creación de tareas mediante un formulario que solicita titulo, responsable, fecha de entrega. Además es posible cambiar el estado arrastrando la tarea a una diferente columna.",
+    capture: "https://firebasestorage.googleapis.com/v0/b/project-test-2eeea.appspot.com/o/landing%2Ftrello-icon2.png?alt=media&token=500c4a2c-cfb9-4a4d-8909-070eca50a3db",
+    developed_in: ["HTML", "Javascript", "CSS"],
+    preview_link: "",
+    github: ""
+  },
+
+  {
+    id: 2,
+    title: "API Super Heroes",
+    description:
+      "Aplicación que consume una Fake API creada en JSON Server y que contiene datos relacionados con los super heroes de Marvel y DC Comics. Se utiliza raect router y animaciones.",
+    capture: "https://firebasestorage.googleapis.com/v0/b/project-test-2eeea.appspot.com/o/landing%2Fsuperhero-icon.png?alt=media&token=641633e1-cadd-452a-92a6-d280dc9635a8",
+    developed_in: ["HTML", "React", "Tailwindcss"],
+    preview_link: "",
+    github: ""
+  },
+
+  {
+    id: 3,
+    title: "Todo APP",
+    description:
+      "Aplicación en la que se implementa un CRUD para crear, editar, actualizar y eliminar una tarea.",
+    capture: "https://firebasestorage.googleapis.com/v0/b/project-test-2eeea.appspot.com/o/landing%2Ftodoapp-icon.png?alt=media&token=4f4f685a-f504-48ed-8168-0beccf8b3cf4",
+    developed_in: ["HTML", "React", "Tailwindcss"],
+    preview_link: "",
+    github: ""
+  },
+];
